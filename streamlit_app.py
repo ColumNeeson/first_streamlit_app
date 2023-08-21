447dce7ce8cc2e5f5f4eecfeceac2a90fb372f6f
ColumNeeson <142583483+ColumNeeson@users.noreply.github.com>
2023-08-21 14:55:45 +0100

    Update streamlit_app.py
    spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
